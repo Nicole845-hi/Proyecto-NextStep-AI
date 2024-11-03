2f655d4fdcd957b033a10a2e2d65ed8c58a8ac9c
Nicole845-hi <seanyujra@gmail.com>
2024-11-03 08:53:02 -0400

    chatbot

---
+++ front.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from chatbot import predict_class, get_response, intents
+st.title("Atencion al Cliente")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "first_message" not in st.session_state:
+    st.session_state.first_message = True
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown (message["content"])
+if st.session_state.first_message:
+    with st.chat_message("assistant"):
+        st.markdown("Hola, ¿cómo puedo ayudarte?")
+    st.session_state.messages.append({"role": "assistant", "content": "Hola, ¿cómo puedo ayudarte?"})
+    st.session_state.first_message = False
+
+
+if prompt := st.chat_input("¿cómo puedo ayudarte?"):
+    with st.chat_message("user"): 
+        st.markdown (prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    #Implementacion del algoritmo de IA 
+    insts = predict_class(prompt)
+    res = get_response(insts, intents)
+    with st.chat_message("assistant"):
+        st.markdown(res)
+    st.session_state.messages.append({"role": "assistant", "content": res})
